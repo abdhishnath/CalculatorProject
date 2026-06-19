4c3aaed293a882a2e6557c2a7eeb78ccf04659a8
Abdhish Kumar Nath <abdhishkumarnath@gmail.com>
2026-06-19 21:23:56 +0530

    first commit

---
+++ Basic_calc.py
@@ -0,0 +1,7 @@
+# This is my first basic calculator
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+Result = num1 + num2
+
+print("The sum is :", Result)
